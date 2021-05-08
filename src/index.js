621982aea92f426094ff2687e72c588f9c55390e
Mikko <mikkovalla@users.noreply.github.com>
2021-05-08 13:25:17 +0300

    TASK 1: fixed endpoint path, added vscode REST call for testing.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 require('./db/mongoose');
-
+const http = require('http')
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 const authRouter = require('./routers/auth');
@@ -11,10 +11,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(userRouter);
+app.use('/api', userRouter);
 app.use(taskRouter);
 app.use(authRouter);
 
-app.listen(port, () => {
+const server = http.createServer(app)
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,12 +4,16 @@ const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
+  console.log(req)
   const user = new User(req.body);
 
   try {
     await user.save();
     const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
+    res.status(201).send({
+      user,
+      token
+    });
   } catch (error) {
     res.status(400).send(error);
   }
