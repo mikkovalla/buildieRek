3fcb940850876a1f1a0c5f9f00e9797a8906ea6d
Mikko <mikkovalla@users.noreply.github.com>
2021-05-08 16:22:22 +0300

    TASK 5: PATCH endpoint added

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,7 +24,9 @@ router.get('/users/me', auth, async (req, res) => {
     const user = await User.findOne({});
     res.status(200).send(user)
   } catch (error) {
-    res.status(400).send({Error: error})
+    res.status(400).send({
+      Error: error
+    })
   }
 });
 
@@ -34,7 +36,28 @@ router.delete("/users/me", auth, async (req, res) => {
     await user.remove()
     res.status(200).send(`User ${user.name} succesfully removed!`)
   } catch (error) {
-    res.status(400).send({Error: error})
+    res.status(400).send({
+      Error: error
+    })
+  }
+})
+
+router.patch("/users/me", auth, async (req, res) => {
+
+  if (!req.body) return res.status(400).send({
+    Error: "No values"
+  })
+  const values = Object.entries(req.body)
+  const reqValues = Object.fromEntries(values)
+
+  try {
+    const user = await User.findOne({})
+    const updatedUser = await User.findByIdAndUpdate(user._id, reqValues)
+    res.status(200).send(`User ${updatedUser.name} succesfully updated!`)
+  } catch (error) {
+    res.status(400).send({
+      Error: error
+    })
   }
 })
 
